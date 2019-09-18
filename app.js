c1ffde3758433900cb431bd177a9f340e577d71e
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-18 16:05:34 -0700

    pulling out current data

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ window.addEventListener("load", () => {
                 })
                 .then(data => {
                     console.log(data);
+                    const {} = data.currently;
             });
         });
     } 
