00c00117cb29e2bab4824c557ef7f8929b815ae8
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-18 15:42:29 -0700

    pulling out info on coordinates

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     let long;
     let lat;
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition
+        (position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        });
+    } 
 });
