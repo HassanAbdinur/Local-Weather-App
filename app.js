7a8a06ef2c1456fc348bcec451bbea526fafc400
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-18 15:57:40 -0700

    working on the api

--- app.js
+++ app.js
@@ -10,7 +10,5 @@ window.addEventListener("load", () => {
 
             const api = `https://api.darksky.net/forecast/1bbc884e3f7c087b75ea6706120e932a/${lat},${long}`;
         });
-
-        
     } 
 });
